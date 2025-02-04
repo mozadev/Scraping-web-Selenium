5fda507e2a140aa02e1c57a6b33f4afd318b004f
mozac <cesarmozareyes@gmail.com>
2025-02-04 17:57:49 -0500

    add code to generate fill word

--- app/modules/words_bots/pronatel/scripts/pronatel.py
+++ app/modules/words_bots/pronatel/scripts/pronatel.py
@@ -3,6 +3,12 @@ import os
 import pandas as pd
 from docxtpl import DocxTemplate
 
+def limpiar_texto(texto):
+    """Limpia caracteres especiales como _x000D_, saltos de línea y espacios innecesarios"""
+    if isinstance(texto, str):  # Verificar si es string
+        return texto.replace("\r", "").replace("\n", "").replace("_x000D_", "").strip()
+    return texto
+
 def fill_word_template(excel_path, word_template_path, word_output_path):
   
     """
@@ -15,45 +21,25 @@ def fill_word_template(excel_path, word_template_path, word_output_path):
     word_outh_path (str) : Ruta donde se guardara el documento word final.
 
     """
-    df_tabla_resumen = pd.read_excel(excel_path, sheet_name="tabla_resumen", dtype=str, engine="openpyxl")
     df_reporte_detalle = pd.read_excel(excel_path, sheet_name="reporte_detalle", dtype=str, engine="openpyxl")
 
-    df_tabla_resumen.columns = df_tabla_resumen.columns.str.strip()
     df_reporte_detalle.columns = df_reporte_detalle.columns.str.strip()
 
-    tabla_resumen = []
-    for _, row in df_tabla_resumen.iterrows():
-    
-        resumen_info = {
-            'ticket': row.get('Ticket', ''),
-            'tipo_generacion': row.get('Tipo de generacion de ticket', ''),
-            'fecha_interrupcion': row.get('Fecha/Hora Inturrepcion', ''),
-            'fecha_solicitud': row.get('Fecha/Hora Solicitud', ''),
-            'fecha_generacion': row.get('Fecha/Hora Generación', ''),
-            'fecha_llegada': row.get('Fecha/Hora Llegada de Personal', ''),
-            'tiempo_llegada': row.get('Tiempo de llegada del Personal (Hrs)', ''),
-            'fecha_subsanacion': row.get('Fecha/Hora Subsanación', ''),
-            'cid': row.get('CID', ''),
-            'tipo_caso': row.get('Tipo Caso', ''),
-            'averia': row.get('Avería', ''),
-            'tiempo_indisponibilidad': row.get('Tiempo de Indisponibilidad - Ver Nota 1 (Hrs)', ''),
-            'tiempo_subsanacion': row.get('Tiempo de subsanación efectivo - Ver Nota 2 (Hrs)', ''),
-            'horas_excedidas': row.get('Horas excedidas en el plazo de reparación de acuerdo a bases (Hrs)', '')
-        }
-        tabla_resumen.append(resumen_info)
+    df_reporte_detalle = df_reporte_detalle.map(limpiar_texto)
 
     reportes_detallados = []
     for _, row in df_reporte_detalle.iterrows():
         reporte = {
-            'nro_ticket': row.get('nro_incidencia', ''),
-            'fecha_gene': row.get('fecha_generacion', ''),
-            'tipo_servicio': row.get('tipo_servicio', ''),
-            'cid': row.get('cid', ''),
+            'nro_ticket': row.get('nro_incidencia', 'no data'),
+            'fecha_gene': row.get('fecha_generacion', 'no data'),
+            'tipo_servicio': row.get('tipo_servicio', 'no data'),
+            'cid': row.get('cid', 'no data'),
             'tipo_caso': row.get('tipo_caso', ''),
-            #'descripcion_problema': row.get('it_descripcion_problema', ''),
-            'determinacion_causa': row.get('it_determinacion_de_la_causa', ''),
-            'medidas_tomadas': row.get('it_medidas_tomadas', ''),
-            'conclusiones': row.get('it_conclusiones', '')
+            'averia': row.get('tipificacion_problema','no data'),
+            'determinacion_causa': row.get('it_determinacion_de_la_causa', 'no data'),
+            'medidas_tomadas': row.get('it_medidas_tomadas', 'no data'),
+            'recomendaciones': row.get('it_conclusiones', 'no data')
+        
         }
         reportes_detallados.append(reporte)
 
@@ -62,13 +48,9 @@ def fill_word_template(excel_path, word_template_path, word_output_path):
 
         'fecha_inicio': "01/01/2025",
         'fecha_fin': "31/01/2025",
-
-        'registros_resumen' : tabla_resumen,
-
         'reportes': reportes_detallados
-
         }
-
+    
     doc.render(context)
     output_file = os.path.join(word_output_path, f"reporte_mensual_completo.docx")
     doc.save(output_file)
@@ -76,11 +58,10 @@ def fill_word_template(excel_path, word_template_path, word_output_path):
 
 if __name__ == "__main__":
 
-    base_path = "C:/Users/katana/Desktop/proyectos/bots_rpa"
-    #base_path = "C:/Users/mozac/Documents/pruebas/Fast-API-Bots-RPA-python-"
-
+    #base_path = "C:/Users/katana/Desktop/proyectos/bots_rpa"
+    base_path = "C:/Users/mozac/Documents/pruebas/Fast-API-Bots-RPA-python-"
     excel_path = os.path.join(base_path, "media", "pronatel", "data", "DataTicketsPronatel.xlsx")
-    word_template_path = os.path.join(base_path, "media", "pronatel", "plantillas", "plantilla_word2.docx")
+    word_template_path = os.path.join(base_path, "media", "pronatel", "plantillas", "plantilla_word5.docx")
     word_output_path = os.path.join(base_path, "media", "pronatel", "reportes/")
 
     fill_word_template(excel_path, word_template_path, word_output_path)
